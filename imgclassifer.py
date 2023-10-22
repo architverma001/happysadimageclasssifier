c2ce0a6025f7bf1754ccbba58c59da62cf903605
Archit verma <102369683+architverma001@users.noreply.github.com>
2023-10-23 02:25:44 +0530

    prepared_model to use camera and predict

--- imgclassifer.py
+++ imgclassifer.py
@@ -1,24 +1,24 @@
 import os
 import tensorflow as tf
-import cv2 as cv
+import cv2
 import numpy as np
 import imghdr
 import matplotlib.pyplot as plt
-
 # Avoid OOM error
 # cpus = tf.config.experimental.list_physical_devices('CPU')
-# print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+# print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU'))
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+# Define data directory and image extensions
 data_dir = 'data'
 image_exts = ['jpg', 'jpeg', 'png', 'bmp', 'gif']
-img = cv.imread(os.path.join(data_dir, 'happy', '35438_hd.jpg'))
-# print(os.listdir(os.path.join(data_dir, 'happy')))
+
+# Iterate through image classes and clean the data
 for image_class in os.listdir(data_dir):
     for image in os.listdir(os.path.join(data_dir, image_class)):
         image_path = os.path.join(data_dir, image_class, image)
         try:
-            img = cv.imread(image_path)
+            img = cv2.imread(image_path)
             tip = imghdr.what(image_path)
             if tip not in image_exts:
                 print("Image not in the list: {}".format(image_path))
@@ -27,43 +27,43 @@ for image_class in os.listdir(data_dir):
             print("Remove error: {}".format(image_path))
             os.remove(image_path)
 
+# Load and preprocess the data
 data = tf.keras.preprocessing.image_dataset_from_directory('data')
-data_iterator = data.as_numpy_iterator()
-batch = next(data_iterator)
-# print(batch[1])
-data = data.map(lambda x,y:(x/255.0, y))
-scaled_iterator = data.as_numpy_iterator()
-batch = next(scaled_iterator)
-print(len(data))
-train_size = int(len(data)*0.7)
-val_size = int(len(data)*0.15)+1
-test_size = int(len(data)*0.15)+1
-print(train_size, val_size, test_size)
+data = data.map(lambda x, y: (x / 255.0, y))
+
+# Split the data into train, validation, and test sets
+train_size = int(len(data) * 0.65)
+val_size = int(len(data) * 0.15) + 1
+test_size = int(len(data) * 0.15) + 1
 
 train = data.take(train_size)
 val = data.skip(train_size).take(val_size)
-test = data.skip(train_size+val_size).take(val_size)
+test = data.skip(train_size + val_size).take(test_size)
 
+# Define the neural network model
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(256, 256, 3)),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
-
-model = Sequential()
-
-model.add(Conv2D(16, 3, padding='same', activation='relu', input_shape=(256, 256, 3)))
-model.add(MaxPool2D())
-model.add(Conv2D(32, 3, padding='same', activation='relu'))
-model.add(MaxPool2D())
-model.add(Conv2D(16, 3, padding='same', activation='relu'))
-model.add(MaxPool2D())
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+# Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Display the model summary
 model.summary()
 
+# Log training data to a directory
 logdir = 'logs'
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir = logdir) 
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+
+# Train the model
 hist = model.fit(train, epochs=30, validation_data=val, callbacks=[tensorboard_callback])
 
 fig = plt.figure(figsize=(10, 5))
@@ -77,3 +77,7 @@ plt.plot(hist.history['accuracy'],label='accuracy', c='red')
 plt.plot(hist.history['val_accuracy'],label='val_accuracy', c='blue')
 plt.legend()
 plt.show()
+
+
+# Save the trained model as an .h5 file
+model.save('imgclassifer.h5')
